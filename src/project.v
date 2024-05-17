5f82e84940748ac8164de70fa65eae317c3de97b
Rowan Goemans <goemansrowan@gmail.com>
2024-05-17 08:53:35 +0200

    Refer to Action that uses openlane with updated yosys

--- src/project.v
+++ src/project.v
@@ -13,7 +13,7 @@ module tt_um_clash_mac (
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       rst_n     // reset_n - low to reset dsdss
 );
 
   topEntity ethmac (
